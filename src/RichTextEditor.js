6aed0daf4621fbc8e10be6369426efc9c256e61e
TharaViswa <32871523+TharaViswa@users.noreply.github.com>
2019-03-12 11:47:33 +0100

    Validate url method added
    Added validate url method.it will get called before insert and it will give an alert to the user to provide valid url

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -246,6 +246,16 @@ export default class RichTextEditor extends Component {
     })
   }
 
+  validateUrl(linkUrl) {
+    const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if(regexp.test(linkUrl)) {
+      this._hideModal();
+      return true
+    } else {
+      Alert.alert('Please provide valid URL')
+    }
+  }
+
   _renderModalButtons() {
     const insertUpdateDisabled = this.state.linkTitle.trim().length <= 0 || this.state.linkUrl.trim().length <= 0;
     const containerPlatformStyle = PlatformIOS ? {justifyContent: 'space-between'} : {paddingTop: 15};
@@ -264,11 +274,12 @@ export default class RichTextEditor extends Component {
         <TouchableOpacity
             onPress={() => {
               if (this._linkIsNew()) {
+                this.validateUrl(this.state.linkUrl)
                 this.insertLink(this.state.linkUrl, this.state.linkTitle);
               } else {
+                this.validateUrl(this.state.linkUrl)
                 this.updateLink(this.state.linkUrl, this.state.linkTitle);
               }
-              this._hideModal();
             }}
             disabled={insertUpdateDisabled}
             style={buttonPlatformStyle}
